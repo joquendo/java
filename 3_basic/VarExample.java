b5368da24373eb398d7e72cd8b6f7dd21150db65
James Oquendo <james.oquendo@gmail.com>
2021-03-12 15:59:33 -0800

    branch update

---
+++ 3_basic/VarExample.java
@@ -0,0 +1,12 @@
+public class VarExample {
+    public static void main (String args[]) {
+        int x = 10;
+        int y = 5;
+        int z;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        z = x + y;
+        System.out.println("x + y = " + z);
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+#### Practical JAVA Programming by Perry Xiao
+
+Exercices following the book.
+
+1_hello-world
+3_basic
+-   Declaring and initializing Java variables
+-   Exercise 3.1- take two integers from command line params, args[0] and args[1] and do calculations
