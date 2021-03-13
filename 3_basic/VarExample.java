d68fbbc738bdb45e646f960b900ec7c70a96d124
James Oquendo <james.oquendo@gmail.com>
2021-03-12 16:22:49 -0800

    Exercise 3.1

--- 3_basic/VarExample.java
+++ 3_basic/VarExample.java
@@ -1,12 +1,18 @@
 public class VarExample {
     public static void main (String args[]) {
-        int x = 10;
-        int y = 5;
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
         int z;
 
         System.out.println("x = " + x);
         System.out.println("y = " + y);
         z = x + y;
         System.out.println("x + y = " + z);
+        z = x - y;
+        System.out.println("x - y = " + z);
+        z = x * y;
+        System.out.println("x * y = " + z);
+        z = x / y;
+        System.out.println("x / y = " + z); // Division results round down in java
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Exercices following the book.
 
-1_hello-world
-3_basic
--   Declaring and initializing Java variables
--   Exercise 3.1- take two integers from command line params, args[0] and args[1] and do calculations
+-   1_hello-world
+-   3_basic
+    -   Declaring and initializing Java variables
+    -   Exercise 3.1 - take two integers from command line params, args[0] and args[1] and do calculations
